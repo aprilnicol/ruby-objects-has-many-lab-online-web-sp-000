6ee0085113d5731eb4510c39cee00f805fed830e
April Adams <illusionlace@gmail.com>
2020-12-18 12:48:02 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,20 +6,20 @@ class Author
     @posts = []
   end
   def posts
-    @posts
+    Post.all.select {|post| post.author = self}
   end
-  def self.post_count
-    @@post_count
-  end
-  def add_post(post_title)
-    @posts << post_title
-    post_title.author = self
+  def add_post(post)
     @@post_count += 1
+    @posts << post 
+    post.author = self 
   end
-  def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
-    post.author = self
+  def add_post_by_title(title)
     @@post_count += 1
+    p = Post.new(title)
+    self.add_post(p)
+    p
   end
+  def self.post_count 
+    @@post_count 
+  end 
 end
