a4b3745e3cbaad5a2064e2d9687abef8058fbd58
April Adams <illusionlace@gmail.com>
2020-12-18 12:07:41 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,17 @@
 class Post
   attr_accessor :author, :title
+  @@all = []
   def initialize(title)
     @title = title
+    @@all << self 
   end
   def author_name
-    if author
-      self.author.name
-    else
-      nil
+    if !@author
+    else 
+      @author.name 
     end
   end
+  def self.all 
+    @all
+  end 
 end 
