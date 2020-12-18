490837d75ea9d641b932bc0ef2374c4f04dbdd18
April Adams <illusionlace@gmail.com>
2020-12-18 12:08:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,17 @@
 class Song
   attr_accessor :artist, :name
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self 
   end
   def artist_name
-    if artist
-      self.artist.name
-    else
-      nil
-    end
-  end
+    if !@artist
+    else 
+      @artist.name 
+    end 
+  end 
+  def self.all
+    @@all 
+  end 
 end 
