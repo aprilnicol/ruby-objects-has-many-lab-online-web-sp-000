3ae3471d79cc2ab7c9dd8873083b186c2cbc960e
April Adams <illusionlace@gmail.com>
2020-12-18 12:03:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,16 +5,13 @@ class Artist
     @name = name
     @songs = []
   end
-  def songs
-    @songs
-  end
   def add_song(song)
     @songs << song
     song.artist = self
     @@song_count += 1
   end
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
     song.artist = self
     @@song_count += 1
