c9c8281052b42e3db86e76a05776aee5fea6e308
April Adams <illusionlace@gmail.com>
2020-12-18 11:59:24 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,9 +15,11 @@ class Author
   end
   def add_post_by_title(post_title)
     post = Post.new(post_title)
+    @posts << post
+    post.author = self
     @@post_count += 1
   end
   def self.post_count
     @@post_count
   end
-end 
+end
