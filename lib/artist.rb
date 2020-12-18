a39d77ab6d6caa3db37726cd28e0ecbd4f5214ac
April Adams <illusionlace@gmail.com>
2020-12-18 12:44:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,25 @@
-class Artist
-  attr_accessor :name
-@@song_count = 0
- def initialize(name)
-    @name = name
+class Artist 
+  attr_accessor :name 
+  @@song_count = 0 
+  def initialize(name)
+    @name = name 
     @songs = []
-  end
-
-  def self.song_count
-    @@song_count
-  end
-
-  def songs
-    @songs
-  end
-
- def add_song(song_name)
-    @songs << song_name
-    song_name.artist = self
-    @@song_count += 1
-  end
-
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
+  end 
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end 
+  def add_song(song)
+    @@song_count += 1 
     @songs << song
-    song.artist = self
-    @@song_count += 1
+    song.artist = self 
+  end 
+  def add_song_by_name(name)
+    @@song_count += 1 
+    the_song =  Song.new(name)
+    self.add_song(the_song)
+    the_song 
+  end 
+  def self.song_count 
+    @@song_count 
   end
 end 
